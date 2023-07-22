c198b7098a85ae89b3f3800c8104ecc9d2324577
huangwei1314 <269150163@qq.com>
2023-07-23 03:29:02 +0800

    Site updated: 2023-07-23 03:29:02

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "CCCCCC",
+      "tmpSize": "16",
+      "cityColor": "EA9999",
+      "citySize": "16",
+      "aqiColor": "CCCCCC",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "1",
+      "language": "auto",
+      "borderRadius": "6",
+      "fixed": "false",
+      "vertical": "center",
+      "horizontal": "center",
+      "key": "1caf57e9523d488993a480456fae3f31"
+    }
+  }
