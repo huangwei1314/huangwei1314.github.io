b38ebcc7ab676f83e665015c55e96fdbc8d6ac3d
huangwei1314 <269150163@qq.com>
2023-09-17 23:06:48 +0800

    Site updated: 2023-09-17 23:06:47

--- js/memos/project.js
+++ js/memos/project.js
@@ -1,21 +1,23 @@
-todolist();
 function todolist() {
-    fetch('https://memos.443.lol/api/v1/memo?creatorId=1&tag=清单').then(res => res.json()).then(data => {
+    fetch('https://memos.443.lol/api/v1/memo?creatorId=1&tag=清单').then(e=>e.json()).then((e=>{
         // 获取并处理数据
-        data.forEach(item => {
+        e.forEach((e=>{
             // 处理数据
-            let content = item.content,
-                title = content.match(/\[(.*?)\]/g)[0].replace(/\[(.*?)\]/, '$1');
+            let t = e.content,
+                l = t.match(/\[(.*?)\]/g)[0].replace(/\[(.*?)\]/, '$1');
             // 去掉多余内容，替换清单内容
-            content = content.replace(/#.*\s/g, '').replace(/(-\s\[\s\]\s)(.*)/g, `<li><i style="margin-right: 5px;" class="fa-regular fa-circle"></i>$2</li>`).replace(/(-\s\[x\]\s)(.*)/g, `<li class="achieve"><i style="margin-right: 5px;" class="fa-regular fa-circle-check"></i>$2</li>`);
+            t = t.replace(/#.*\s/g, '').replace(/(-\s\[\s\]\s)(.*)/g, `<li><i style="margin-right: 5px;" class="fa-regular fa-circle"></i>$2</li>`).replace(/(-\s\[x\]\s)(.*)/g, `<li class="achieve"><i style="margin-right: 5px;" class="fa-regular fa-circle-check"></i>$2</li>`);
             // 渲染数据
-            let div = document.createElement('div');
-            div.className = 'list_item';
-            div.innerHTML = `<h3>${title}</h3><ul>${content}</ul>`;
-            document.getElementById('todolist').appendChild(div);
-        });
+            let a = document.createElement('div');
+            a.className = 'list_item';
+            a.innerHTML = `<h3>${1}</h3><ul>${t}</ul>`;
+            document.getElementById('todolist').appendChild(a)
+        }
+        ));
         waterfall('#todolist');
-    }).catch()
+    }    
+    )).catch()
 }
+todolist();
 
 // 清单函数
