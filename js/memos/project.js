56b6e9e22b4c54c9de35645217b50fe11a7a205a
huangwei1314 <269150163@qq.com>
2023-09-22 19:44:42 +0800

    Site updated: 2023-09-22 19:44:42

--- js/memos/project.js
+++ js/memos/project.js
@@ -1,20 +1,27 @@
-function todolist() {
-    fetch('https://memos.443.lol/api/v1/memo?creatorId=1&tag=清单').then(res => res.json()).then(data => {
-        // 获取并处理数据
-        data.forEach(item => {
-            // 处理数据
-            let content = item.content,
-                title = content.match(/\[(.*?)\]/g)[0].replace(/\[(.*?)\]/, '$1');
-            // 去掉多余内容，替换清单内容
-            content = content.replace(/#.*\s/g, '').replace(/(-\s\[\s\]\s)(.*)/g, `<li><i style="margin-right: 5px;" class="fa-regular fa-circle"></i>$2</li>`).replace(/(-\s\[x\]\s)(.*)/g, `<li class="achieve"><i style="margin-right: 5px;" class="fa-regular fa-circle-check"></i>$2</li>`);
-            // 渲染数据
-            let a = document.createElement('div');
-            a.className = 'list_item';
-            a.innerHTML = `<h3>${title}</h3><ul>${content}</ul>`;
-            document.getElementById('todolist').appendChild(a);
-        });
-        waterfall('#todolist');
-    }).catch()
-}
-todolist();
-// 清单函数
+async function todolist() {
+    try {
+      const response = await fetch('https://memos.443.lol/api/v1/memo?creatorId=1&tag=清单');
+      const data = await response.json();
+      
+      data.forEach(item => {
+        let content = item.content;
+        let title = content.match(/\[(.*?)\]/g)[0].replace(/\[(.*?)\]/, '$1');
+        
+        content = content.replace(/#.*\s/g, '')
+                         .replace(/(-\s\[\s\]\s)(.*)/g, `<li><i style="margin-right: 5px;" class="fa-regular fa-circle"></i>$2</li>`)
+                         .replace(/(-\s\[x\]\s)(.*)/g, `<li class="achieve"><i style="margin-right: 5px;" class="fa-regular fa-circle-check"></i>$2</li>`);
+        
+        const div = document.createElement('div');
+        div.className = 'list_item';
+        div.innerHTML = `<h3>${title}</h3><ul>${content}</ul>`;
+        document.getElementById('todolist').appendChild(div);
+      });
+      
+      waterfall('#todolist');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  todolist();
+  // 清单函数
